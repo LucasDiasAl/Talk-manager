86c8da64a3d69369b98bb2194904774a035f27cc
Lucas Dias <lucaalencarID@gmail.com>
2023-01-25 17:24:52 -0300

    requisito 2 add a requisiçao por um um id especifico atraves da rota /talker/:id

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -2,19 +2,29 @@ const express = require('express');
 
 const { readTalkers } = require('../utils/fsUtils');
 
+const HTTP_OK_STATUS = 200;
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
     const talkers = await readTalkers();
-    res.status(200).json([...talkers]);
+    res.status(HTTP_OK_STATUS).json([...talkers]);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
-// router.get('/:id', async (req, res) => {
-
-// });
+router.get('/:id', async (req, res) => {
+  try {
+    const talker = (await readTalkers()).filter(({ id }) => id === Number(req.params.id));
+    if (talker.length === 0) {
+      throw new Error('Pessoa palestrante não encontrada');
+    }
+    res.status(HTTP_OK_STATUS).json(...talker);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+});
 
 module.exports = router;
